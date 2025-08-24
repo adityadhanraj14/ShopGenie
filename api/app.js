fd6073601b219d48f55e3ca6ffc3a374ef5bc422
aditya dhanraj <adityadhanraj576@gmail.com>
2025-08-24 08:58:43 +0530

    remove mongooese connect urlparser

--- api/app.js
+++ api/app.js
@@ -23,10 +23,7 @@ const connectToMongoDB = async () => {
         return;
     }
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(process.env.MONGODB_URI);
         isConnected = true;
         console.log("Connected to MongoDB successfully");
     } catch (error) {
